d0e8fc8c2d41fcd152d4d51c90f9723719663d61
warisamir <77074869+warisamir@users.noreply.github.com>
2022-05-12 00:00:16 +0530

    Create Pattern14.java

---
+++ Patterns/Pattern14.java
@@ -0,0 +1,46 @@
+1. You are given a number n. 2. You've to write code to print it's multiplication table up to 10 in format given below.
+
+
+Constraints
+1 <= n <= 10
+
+Format
+Input
+A number x
+
+Output
+pat141
+
+Example
+Sample Input
+
+3
+
+Sample Output
+3 * 1 = 3
+3 * 2 = 6
+3 * 3 = 9
+3 * 4 = 12
+3 * 5 = 15
+3 * 6 = 18
+3 * 7 = 21
+3 * 8 = 24
+3 * 9 = 27
+3 * 10 = 30
+  
+  //
+  import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+int h;
+        // write iur code here
+int n=scn.nextInt();
+for(int i=1;i<=10;i++){
+    h=n*i;
+    System.out.print(n+" * "+i+" = "+h+"\n");
+} System.out.println();
+    }
+}
