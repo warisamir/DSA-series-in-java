2d73226faf6322e559764e7a42c198233e27f9fe
warisamir <77074869+warisamir@users.noreply.github.com>
2022-05-12 00:43:25 +0530

    Create Printzigzag.java

---
+++ RecursionandBacktracking/Printzigzag.java
@@ -0,0 +1,59 @@
+Print Zigzag
+Easy
+
+1. Here are a few sets of inputs and outputs for your reference
+Input1 -> 1
+Output1 -> 1 1 1
+
+Input2 -> 2
+Output2 -> 2 1 1 1 2 1 1 1 2
+
+Input2 -> 3
+Output3 -> 3 2 1 1 1 2 1 1 1 2 3 2 1 1 1 2 1 1 1 2 3
+
+2. Figure out the pattern and complete the recursive function pzz to achieve the above for any positive number n.
+
+Note -> The online judge can't force you to write the function recursively but that is what the spirit of question is.Write recursive and not iterative logic. The purpose of the question is to aid learning recursion and not test you.
+
+Constraints
+1 <= n <= 10
+
+Format
+Input
+A number n
+
+Output
+As discussed in point 1 of description
+
+Example
+Sample Input
+
+3
+
+Sample Output
+3 2 1 1 1 2 1 1 1 2 3 2 1 1 1 2 1 1 1 2 3
+  
+  //
+  import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+         pzz(n);
+    }
+
+    public static void pzz(int n){
+        if(n==0)
+        return ;
+        System.out.print(n+" ");
+        pzz(n-1);
+        System.out.print(n+" ");
+        pzz(n-1);
+        System.out.print(n+" ");
+    }
+
+}
