5c5e23fb0486bff780426b3ff2bca416937b3bd9
warisamir <77074869+warisamir@users.noreply.github.com>
2022-05-16 15:42:36 +0530

    Time: 19 ms (26.76%), Space: 48.2 MB (42.69%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.java
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.java
@@ -1,30 +1,66 @@
 class Solution {
     public int findPairs(int[] nums, int k) {
-          HashMap<Integer,Integer> hm=new  HashMap<>();
-     int p=0,q=1;
-        int count=0;
-        int len=nums.length;
-        while(p<len && q<len){
-         int diff=Math.abs(nums[p]-nums[q]);
-            if(diff==k){
-                int x,y;
-                if(nums[p]>nums[q])
-                {
-                    x=nums[p];
-                    y=nums[q];
-                }
-                else
-                {     x=nums[q];
-                y=nums[p];
+//           HashMap<Integer,Integer> hm=new  HashMap<>();
+//      int p=0,q=1;
+//         int count=0;
+//         int len=nums.length;
+//         while(p<len && q<len){
+//          int diff=Math.abs(nums[p]-nums[q]);
+//             if(diff==k){
+//                 int x,y;
+//                 if(nums[p]>nums[q])
+//                 {
+//                     x=nums[p];
+//                     y=nums[q];
+//                 }
+//                 else
+//                 {     x=nums[q];
+//                 y=nums[p];
+//             }
+//                 hm.put(x,y);
+//         }
+//             if(q==len-1){
+//             p++;
+//             q=p+1;
+//             }
+//             else q++;
+//         }
+//         return hm.size();
+//     }
+// }
+    
+       HashMap<Integer,Integer> hm=new HashMap<>();
+        int l=nums.length;
+        int par=0;
+        for(int i=0;i<nums.length;i++){
+            if(hm.containsKey(nums[i])){
+                hm.put(nums[i],hm.get(nums[i])+1);
             }
-                hm.put(x,y);
+            else
+                hm.put(nums[i],1);
         }
-            if(q==len-1){
-            p++;
-            q=p+1;
-            }
-            else q++;
+        for(Map.Entry m:hm.entrySet()){
+            int key=(int)m.getKey();
+            int val=(int)m.getValue();
+            int fk=k+key;
+      
+            if(k==0 && val>1)
+             par++;
+            if(k!=0 && hm.containsKey(fk))
+                par++;
         }
-        return hm.size();
+return par;
+        }
     }
-}
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+      
