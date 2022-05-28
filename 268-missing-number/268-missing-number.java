9e07c3f222d49c6b5724b6dcaf12aa55b3aa4def
warisamir <77074869+warisamir@users.noreply.github.com>
2022-05-28 10:38:54 +0530

    Time: 1 ms (65.97%), Space: 51.1 MB (48.77%) - LeetHub

---
+++ 268-missing-number/268-missing-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+       int n=nums.length;
+ int sum=0;
+       for(int i=0;i<nums.length;i++){
+           sum+=nums[i];
+       }
+        return(n*(n+1)/2)-sum;
+    }
+}
