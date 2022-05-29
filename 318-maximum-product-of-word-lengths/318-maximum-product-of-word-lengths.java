ef01d52661083664ef48289ad70b3992a7679e63
warisamir <77074869+warisamir@users.noreply.github.com>
2022-05-29 13:32:46 +0530

    Time: 11 ms (71.82%), Space: 45.9 MB (66.48%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.java
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.java
@@ -1,32 +1,20 @@
 class Solution {
     public int maxProduct(String[] word) {
-       int n=word.length;
-        List<Set<Character>>ls=new ArrayList(n);
+
+   int n=word.length;
+        int []bm=new int[n];
         int mx=0;
-        for(String w:word){
-            Set<Character>set=new HashSet();
-            ls.add(set);
-            for(int i=0;i<w.length();i++){
-                set.add(w.charAt(i));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<word[i].length();j++){
+                int id=word[i].charAt(j)-'a';
+                bm[i] |=(1<<id);
+            }
+            for(int j=0;j<i;j++){
+                if((bm[i] &bm[j])==0)
+                    mx=Math.max(mx,word[i].length()*word[j].length());
             }
         }
-          
-        for(int i=0;i<n;i++){
-            Set<Character>set1=ls.get(i);
-            for(int j=i+1;j<n;j++){
-                boolean com=false;
-                Set<Character>set2=ls.get(j);
-                for(Character c:set1){
-                    if(set2.contains(c)){
-                        com=true;
-                        break;
-                    }
-                }
-            
-            if(!com)
- mx=Math.max(mx,word[i].length()*word[j].length());
-        }
-        }
-        return mx;
+                               return mx;
     }
 }
+    
