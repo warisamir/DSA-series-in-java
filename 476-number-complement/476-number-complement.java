b7b8ca9b3a9ea5702dc257a489bce7c8f04209a4
warisamir <77074869+warisamir@users.noreply.github.com>
2022-06-23 07:20:53 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (89.88%) - LeetHub

--- 476-number-complement/476-number-complement.java
+++ 476-number-complement/476-number-complement.java
@@ -1,6 +1,13 @@
 class Solution {
     public int findComplement(int num) {
-        return ~num & (Integer.highestOneBit(num)-1);
-        
+        int m=num;
+        int mask=0;
+        if(num==0)return 1;
+        while(m!=0){
+            mask=(mask<<1)|1;
+            m>>=1;
+        }
+        int nm=~num&mask;
+        return nm;
     }
 }
