1590565f238e381fd6ce171f02ea3ceb76832f80
warisamir <77074869+warisamir@users.noreply.github.com>
2022-07-26 14:54:13 +0530

    Time: 2 ms (45.33%), Space: 45.1 MB (46.93%) - LeetHub

--- 768-max-chunks-to-make-sorted-ii/768-max-chunks-to-make-sorted-ii.java
+++ 768-max-chunks-to-make-sorted-ii/768-max-chunks-to-make-sorted-ii.java
@@ -1,18 +1,20 @@
 class Solution {
     public int maxChunksToSorted(int[] arr) {
         int n=arr.length;
-       int min[]=new int[n];
+        int min[]=new int[n];
         min[n-1]=arr[n-1];
+        int count=1;
         for(int i=arr.length-2;i>=0;i--){
-            min[i]=Math.min(min[i+1],arr[i]);
+            min[i]=Math.min(arr[i],min[i+1]);
         }
-        int re=0;
         int max=Integer.MIN_VALUE;
         for(int i=0;i<arr.length-1;i++){
-            max=Math.max(max,arr[i]);
-            if(max<=min[i+1])
-                re++;
+            max=Math.max(arr[i],max);
+            if(max<=min[i+1]){
+                count++;
+                
+            }
         }
-        return re+1;
+        return count;
     }
 }
