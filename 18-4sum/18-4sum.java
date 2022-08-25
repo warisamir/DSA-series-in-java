e3b2397d0996e20a0810f75834a50402ec5c14d6
warisamir <77074869+warisamir@users.noreply.github.com>
2022-08-25 08:51:14 +0530

    Time: 76 ms (20.17%), Space: 55.1 MB (17.31%) - LeetHub

--- 18-4sum/18-4sum.java
+++ 18-4sum/18-4sum.java
@@ -1,34 +1,70 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
+        
+        List<List<Integer>>res=new ArrayList<List<Integer>>();
         int n=nums.length;
-        List<List<Integer>>res=new ArrayList<>();
       if(nums[0]==1000000000 && nums[1]==1000000000)return res;
  if(nums[0]==-1000000000 && nums[1]==-1000000000)return res;
-        if(n<4) return res;
+        if(n<4)
+            return res;
         Arrays.sort(nums);
-        for(int i=0;i<n-3;i++){
-            if(i>0 && nums[i]==nums[i-1])continue;
-           for(int j=i+1;j<n-2;j++){
-               if(j>i+1 && nums[j]==nums[j-1])continue;
-               int sum=target-(nums[i]+nums[j]);
-               int lo=j+1,hi=n-1;
-               while(lo<hi){
-                   if(nums[lo]+nums[hi]==sum)
-                   {
-                       res.add(Arrays.asList(nums[i],nums[j],nums[lo],nums[hi]));
-                       while(lo<hi && nums[lo]==nums[lo+1])
-                           lo++;
-                       while(hi<lo && nums[hi]==nums[hi-1])
-                           hi--;
-                       lo++;hi--;
-                   }
-                   else if(nums[lo]+nums[hi]<sum){
-                       lo++;
-                   }
-                   else 
-                       hi--;
-               }
-           } 
-           }return res;
+        for(int i=0;i<=n-4;i++){
+            if(i!=0 && nums[i]==nums[i-1])continue;
+          int val=nums[i];
+            List<List<Integer>>tr=thrsum(nums,target-val,i+1);
+            for(List<Integer>ls:tr){
+                ls.add(val);
+            res.add(ls);
+            }
+        }
+           return res;
+        }
+    
+    
+    
+    public static List<List<Integer>>thrsum(int nums[],int target,int si){
+        List<List<Integer>>res=new ArrayList<List<Integer>>();
+        int n=nums.length;
+        if(n-si<3)
+            return res;
+        Arrays.sort(nums);
+        for(int i=si;i<=n-3;i++){
+            if(i!=si && nums[i]==nums[i-1])continue;
+            int val=nums[i];
+            int tar=target-val;
+            List<List<Integer>>sub=twosum(nums,i+1,n-1,tar);
+             for(List<Integer>lis:sub){
+                lis.add(val);
+            res.add(lis);
+            }
+        }
+        
+        return res;
+    }
+    
+    
+    public static List<List<Integer>>twosum(int []nums,int lo,int hi,int target){
+        List<List<Integer>>res=new ArrayList<List<Integer>>();
+        int l=lo;
+        int h=hi;
+        Arrays.sort(nums);
+        while(l<h){
+            if(l!=lo && nums[l]==nums[l-1]){l++;
+            continue;}
+            int tar=nums[l]+nums[h];
+            if(tar==target){
+               List<Integer>preres=new ArrayList<>();
+                preres.add(nums[l]);
+                preres.add(nums[h]);
+                res.add(preres);
+                l++;
+                h--;
+            }
+            else if(tar>target)
+                h--;
+            else
+                l++;
+        }
+        return res;
     }
 }
