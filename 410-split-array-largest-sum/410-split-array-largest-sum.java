07cd4fa63fcd3d2e156e75a61a7308ceb6842a63
warisamir <77074869+warisamir@users.noreply.github.com>
2022-09-08 23:31:44 +0530

    Time: 4 ms (20.39%), Space: 41.4 MB (75.10%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.java
+++ 410-split-array-largest-sum/410-split-array-largest-sum.java
@@ -1,36 +1,29 @@
 class Solution {
     public int splitArray(int[] nums, int m) {
-        int n=nums.length;
-        int res=Integer.MAX_VALUE;
-        int lef=0,rig=0;
-        for(int x:nums)
-        { lef=Math.max(lef,x);
-        rig+=x;}
-        int sum=rig;
-        for(int x:nums){
-            int mid=(lef+rig)/2;
-            int ans=1,curr=0,currmax=0;
-            for(int c:nums){
-                if(c+curr>mid){
-                    ans++;
-                    currmax=Math.max(curr,currmax);
-                    curr=0;
-                }
-                curr+=c;
-            }
-            currmax=Math.max(curr,currmax);
-            
-            if(ans>m){
-                lef=mid+1;
-            }
-            else{
-                rig=mid;
-                if(ans==m){
-                    res=Math.min(res,currmax);
-                }
+       int lo=Integer.MIN_VALUE;
+        int hi=0;
+        for(int num:nums){
+            hi+=num;
+            lo=Math.max(lo,num);
+        }
+        while(lo<hi){
+            int mid=(lo+hi)/2;
+            int limit=mid;
+            int cp=1,cpsum=0;
+            for(int num:nums){
+            if(cpsum+num<=limit){
+                cpsum+=num ;
+            }else
+            {
+                cpsum=num;
+                cp++;
             }
         }
-            return res==Integer.MAX_VALUE?lef:res;
-        
-    }
+        if(cp>m)
+            lo=mid+1;
+        else
+            hi=mid;
+        }
+    return lo;
+}
 }
