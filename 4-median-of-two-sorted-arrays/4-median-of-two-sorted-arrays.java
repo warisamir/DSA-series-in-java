e0abf8e8d689a3b52b366953c5ee87533fcaed01
warisamir <77074869+warisamir@users.noreply.github.com>
2022-09-13 13:57:28 +0530

    Time: 4 ms (71.43%), Space: 49 MB (83.75%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
@@ -1,25 +1,30 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int i=0 , j=0 , k=0;
-        double ans = 0.0;
-        int narr[] = new int[nums1.length + nums2.length];
-        
-        while(nums1.length>i && nums2.length>j){
-            if(nums1[i]<nums2[j])
-                narr[k++]=nums1[i++];
-            else
-                narr[k++]=nums2[j++];
-        }
-        while(nums1.length>i)
-            narr[k++]=nums1[i++];
-        while(nums2.length>j)
-            narr[k++]=nums2[j++];
-        double ct=0;
-        int med=narr.length/2;
-        if(narr.length%2==0)
-            ct=(narr[med]+narr[med-1])/2.0;
-        else
-            ct=narr[med];
-        return ct;
+       int m=nums1.length;
+        int n=nums2.length;
+        int m1=(m+n+1)/2;
+        int m2=(m+n+2)/2;
+        int mid1=merge(nums1,nums2,m1,0,0);
+        int mid2=merge(nums1,nums2,m2,0,0);
+        return (mid1+mid2)/2.0;
     }
+   public int merge(int nums1[],int []nums2,int k,int s1,int s2){
+       if(s1>=nums1.length)
+           return nums2[s2+k-1];
+      else if(s2>=nums2.length)
+           return nums1[s1+k-1];
+       if(k==1)
+          return Math.min(nums1[s1],nums2[s2]);
+        int el1=Integer.MAX_VALUE;
+        int el2=Integer.MAX_VALUE;
+       if(s1+k/2-1<nums1.length)
+       el1=nums1[s1+k/2-1];
+       if(s2+k/2-1<nums2.length)
+        el2=nums2[s2+k/2-1];
+       if(el1<el2)
+        return  merge(nums1,nums2,k-k/2,s1+k/2,s2);
+       else
+          return  merge(nums1,nums2,k-k/2,s1,s2+k/2);
+       
+   }
 }
