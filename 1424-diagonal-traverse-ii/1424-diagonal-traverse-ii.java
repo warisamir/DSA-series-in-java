e9e30b617992629e00f6e2c0aeb7178a5cd0cf3d
warisamir <77074869+warisamir@users.noreply.github.com>
2022-09-20 22:43:11 +0530

    Time: 77 ms (67.17%), Space: 112.1 MB (75.68%) - LeetHub

--- 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.java
+++ 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.java
@@ -1,21 +1,36 @@
 class Solution {
+    class Pair{
+        int i;int val;int sum;
+        public Pair(int i,int val,int sum){
+            this.i=i;
+            this.val=val;
+            this.sum=sum;
+        }
+    }
     public int[] findDiagonalOrder(List<List<Integer>> nums) {
-     List<Integer> l=new ArrayList<>();
-        Map<Integer,List<Integer>>hm=new HashMap<>();
-        int maxKey=0;
-        for(int i=0;i<nums.size();++i)
-            for(int j=0;j<nums.get(i).size();++j){
-              final  int key=i+j;
-                hm.putIfAbsent(key,new ArrayList<>());
-                hm.get(key).add(nums.get(i).get(j));
-                 maxKey=Math.max(key,maxKey);
+        int size=getsize(nums);
+        Pair [] arr=new Pair[size];
+        int idx=0;
+        for(int i=0;i<nums.size();i++){
+            for(int j=0;j<nums.get(i).size();j++){
+                Pair tem=new Pair(i,nums.get(i).get(j),i+j);
+                arr[idx++]=tem;
             }
-            for(int i=0;i<=maxKey;i++){
-                for(int j=hm.get(i).size()-1;j>=0;j--)
-                    l.add(hm.get(i).get(j));
-            }
-        
-        return l.stream().mapToInt(i->i).toArray();
-    
+        }
+        Arrays.sort(arr,(a,b)->{ 
+            if(a.sum!=b.sum) return a.sum-b.sum ;
+            return b.i-a.i;});
+        int ans[]=new int[size];
+        int i=0;
+        for(Pair p:arr){
+            ans[i++]=p.val;
+        }
+        return ans;
+    }
+    public int getsize(List<List<Integer>>ls){
+        int size=0;
+        for(List<Integer>val:ls)
+            size+=val.size();
+        return size;
     }
 }
