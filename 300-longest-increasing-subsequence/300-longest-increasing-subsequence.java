9334f64e97fb11f3ad06dd8797c6f8053416fa03
warisamir <77074869+warisamir@users.noreply.github.com>
2022-09-25 22:56:34 +0530

    Time: 3 ms (98.71%), Space: 44.3 MB (62.48%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,18 +1,23 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int lis[]=new int[nums.length];
-        lis[0]=1;
+        lis[0]=nums[0];
+        int l=1;
         for(int i=1;i<nums.length;i++){
-            int max=0;
-            for(int j=0;j<i;j++){
-                if(nums[j]<nums[i])
-                    max=Math.max(lis[j],max);
-            } lis[i]=max+1;
-        }
-       int l=0;
-        for(int val:lis){
-            l=Math.max(val,l);
-        }
+                if(nums[i]>lis[l-1])
+                { l++;
+             lis[l-1]=nums[i];
+                }
+            else{
+                int idx=Arrays.binarySearch(lis,0,l-1,nums[i]);
+                if(idx<0){
+                    idx=-idx;
+                    idx=idx-1;
+                }
+                lis[idx]=nums[i];
+            }
+            }
+     
         return l;
     }
 }
