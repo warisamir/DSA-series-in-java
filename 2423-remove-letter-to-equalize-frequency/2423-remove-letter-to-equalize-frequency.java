c1cb8de7f1562070ff8363830614d9b16291f237
warisamir <77074869+warisamir@users.noreply.github.com>
2022-10-03 23:59:19 +0530

    Time: 2 ms (100.00%), Space: 42.3 MB (100.00%) - LeetHub

--- 2423-remove-letter-to-equalize-frequency/2423-remove-letter-to-equalize-frequency.java
+++ 2423-remove-letter-to-equalize-frequency/2423-remove-letter-to-equalize-frequency.java
@@ -1,19 +1,26 @@
 class Solution {
     public boolean equalFrequency(String word) {
-        HashMap<Character,Integer>hm=new HashMap<>();
-        for(int i=0;i<word.length();i++){
-            hm.put(word.charAt(i),hm.getOrDefault(word.charAt(i),0)+1);
+       int ar[]=new  int[26];
+        for(char c:word.toCharArray()){
+            ar[c-'a']++;
         }
-        for(int i=0;i<word.length();i++){
-            char c=word.charAt(i);
-            if(hm.get(c)==1)
-                hm.remove(c);
-                else 
-                    hm.put(c,hm.get(c)-1);
-            Set<Integer>hs=new HashSet<>(hm.values());
-            if(hs.size()==1)return true;
-            hm.put(c,hm.getOrDefault(c,0)+1);
+        for(int i=0;i<26;i++){
+            if(ar[i]==0) 
+                continue;
+                ar[i]--;
+        if (res(ar))
+            return true;
+            ar[i]++;
         }
         return false;
     }
+    public boolean res(int arr[]){
+        HashSet<Integer>hm=new HashSet<>();
+        for(int i=0;i<26;i++){
+            if(arr[i]!=0)
+            hm.add(arr[i]);
+        }
+        return hm.size()<2;
+    }
+    
 }
