db31de5d708c6182f07750a32bd933bd42bc9443
warisamir <77074869+warisamir@users.noreply.github.com>
2022-10-04 20:14:35 +0530

    Time: 4 ms (22.66%), Space: 44 MB (65.01%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,10 +1,10 @@
 class Solution {
     public int maxProfit(int[] prices) {
        int pro=0;
-        for(int i=0;i<prices.length-1;i++){
-            if(prices[i]<prices[i+1])
-                pro+=prices[i+1]-prices[i];
-        }
-    return pro;
+       for(int i=0;i<prices.length-1;i++){
+           if(prices[i]<prices[i+1])
+               pro+=prices[i+1]-prices[i];
+       }
+        return pro;
     }
 }
