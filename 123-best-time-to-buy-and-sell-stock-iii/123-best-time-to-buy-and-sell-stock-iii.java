c8bffc663593310da7ee14b1808d6ec5e7313a18
warisamir <77074869+warisamir@users.noreply.github.com>
2022-10-05 21:12:10 +0530

    Time: 4 ms (91.57%), Space: 51.1 MB (98.96%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,29 +1,28 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int left[]=new int[prices.length];
-        int righ[]=new int[prices.length];
-        int buy=prices[0];
+       int dp[]=new int[prices.length];
+        //dpstarting  = profit if sold today or before today
+        dp[0]=0;
+        int lpsf=prices[0];
         for(int i=1;i<prices.length;i++){
-            int pro=prices[i]-buy;
-            if(left[i-1]>pro)
-                pro=left[i-1];
-            left[i]=pro;
-            buy=Math.min(prices[i],buy);
+            lpsf=Math.min(lpsf,prices[i]);
+            int pist=prices[i]-lpsf;
+            dp[i]=Math.max(dp[i-1],pist);
         }
-        int sell=prices[prices.length-1];
+        //dpbaad  = store profit if bought today or after today 
+        int dpb[]=new int[prices.length];
+        dpb[dpb.length-1]=0;
+        int mpsf=prices[prices.length-1];
         for(int i=prices.length-2;i>=0;i--){
-            int p=sell-prices[i];
-            if(righ[i+1]>p)
-                p=righ[i+1];
-            righ[i]=p;
-            sell=Math.max(sell,prices[i]);
+          mpsf=Math.max(mpsf,prices[i]);
+            int prof=mpsf-prices[i];
+            dpb[i]=Math.max(dpb[i+1],prof);
         }
-        int maxpor=0;
+        //dp+dpb= 
+        int val=0;
         for(int i=0;i<prices.length;i++){
-            int curr=left[i]+righ[i];
-            if(curr>maxpor)
-                maxpor=curr;
+            val=Math.max(dp[i]+dpb[i],val);
         }
-        return maxpor;
+        return val;
     }
 }
