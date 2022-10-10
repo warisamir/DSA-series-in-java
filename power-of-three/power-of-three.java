62528269adedd3bff6522d65dbd06aa7bf3d05ac
VIVEK1918YADAV <vivekyadav86706@gmail.com>
2022-10-11 01:33:47 +0530

    cetr

--- power-of-three/power-of-three.java
+++ power-of-three/power-of-three.java
@@ -1,6 +1,15 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        if(n<=0)return false;
-      return (1162261467%n==0);
+      if(n == 0) {
+        return false;
     }
+    if(n == 1) {
+        return true;
+    }
+    
+    if(n%3 == 0) {
+        return isPowerOfThree(n/3);
+    }
+    return false;
+}
 }
