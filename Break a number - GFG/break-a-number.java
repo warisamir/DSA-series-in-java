02e8d16f63a5c1876291a412e96fabf7f8b7397e
WarisAmir <warisamir1918@yahoo.com>
2022-11-09 00:49:56 +0530

    Added solution - LeetHub

---
+++ Break a number - GFG/break-a-number.java
@@ -0,0 +1,29 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+
+            Solution ob = new Solution();
+            System.out.println(ob.waysToBreakNumber(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution{
+    static int waysToBreakNumber(int n){
+        // code here
+        n++;
+        return n*(n+1)/2;
+    }
+}
