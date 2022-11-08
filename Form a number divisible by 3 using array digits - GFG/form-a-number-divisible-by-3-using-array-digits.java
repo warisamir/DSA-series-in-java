89d313fcf6fb9b327f437305e25378d51913296f
WarisAmir <warisamir1918@yahoo.com>
2022-11-09 01:07:49 +0530

    Added solution - LeetHub

---
+++ Form a number divisible by 3 using array digits - GFG/form-a-number-divisible-by-3-using-array-digits.java
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader in =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(in.readLine());
+            String a[] = in.readLine().trim().split("\\s+");
+            int arr[] = new int[N];
+            for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(a[i]);
+
+            Solution ob = new Solution();
+            System.out.println(ob.isPossible(N, arr));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static int isPossible(int N, int arr[]) {
+        // code here)
+        //using modular arithmetic if(A%c+ b%c)=(Ab)%c
+     int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i]%3;
+        }
+        return (sum)%3==0?1:0;
+    }
+}
