c241e7d807eae664e1f4dad3c80418d103cd5e90
WarisAmir <warisamir1918@yahoo.com>
2022-11-13 20:33:55 +0530

    Added solution - LeetHub

---
+++ Factorial - GFG/factorial.java
@@ -0,0 +1,28 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+            System.out.println(ob.factorial(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution{
+    static long factorial(int n){
+        // code here
+        if(n==1 || n==0)return 1;
+        return factorial(n-1)*n;
+    }
+}
