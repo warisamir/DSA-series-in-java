645ba59efac319d720ccc54b446c60ca469f3d7d
WarisAmir <warisamir1918@yahoo.com>
2022-11-24 02:59:32 +0530

    Added solution - LeetHub

---
+++ Magic Triplets - GFG/magic-triplets.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            String s = br.readLine().trim();
+            String[] S = s.split(" ");
+            int[] nums = new int[n];
+            for(int i = 0; i < n; i++){
+                nums[i] = Integer.parseInt(S[i]);
+            }
+            Solution ob = new Solution();
+            int ans = ob.countTriplets(nums);
+            System.out.println(ans);         
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    public int countTriplets(int[] nums){
+        // code here
+       int gt=0;
+        for(int i=1;i<nums.length-1;i++){
+            int ct=0;
+            for(int j=i-1;j>=0;j--){
+                if(nums[i]>nums[j])
+                ct++;
+            }
+         int ans=0;
+        for(int j=i+1;j<nums.length;j++){
+            if(nums[i]<nums[j])
+            ans++;
+        }
+        gt+=ans*ct;
+       
+    } return (gt);
+}
+}
