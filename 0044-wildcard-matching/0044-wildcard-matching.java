c201165c1be1febb0ce089587414fd330de02f16
WarisAmir <warisamir1918@yahoo.com>
2022-11-23 19:16:04 +0530

    Time: 57 ms (27.49%), Space: 48.9 MB (40.07%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,30 +1,32 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        
-    boolean dp[][]=new boolean[p.length()+1][s.length()+1];
-       for(int i=dp.length-1;i>=0;i--){
-           for(int j=dp[0].length-1;j>=0;j--){
-               if(i==dp.length-1 && j==dp[0].length-1)
-                   dp[i][j]=true;
-               else if(i==dp.length-1)
-                   dp[i][j]=false;
-               else if(j==dp[0].length-1)
-               { if(p.charAt(i)=='*')
-                       dp[i][j]=dp[i+1][j];
-              else {
-               dp[i][j]=false;
+        int n=s.length();
+        int m=p.length();
+    boolean dp[][]=new boolean[s.length()+1][p.length()+1];
+       for(int i=n;i>=0;i--){
+           for(int j=m;j>=0;j--){
+               if(i==n && j==m)
+                 dp[i][j]=true;
+               else if(i==n)
+               {
+                 if(p.charAt(j)=='*')
+                     dp[i][j]=dp[i][j+1];
+                   else
+                       dp[i][j]=false;
                }
-               }
+              else if(j==m)
+              {
+                  dp[i][j]=false;
+              }
                else{
-                   if(p.charAt(i)=='?')
+                   if(s.charAt(i)==p.charAt(j) || p.charAt(j)=='?')
                        dp[i][j]=dp[i+1][j+1];
-                       else if(p.charAt(i)=='*')
-                           dp[i][j]=dp[i+1][j]||dp[i][j+1];
-                           else if(p.charAt(i)==s.charAt(j))
-                             dp[i][j]=dp[i+1][j+1] ; 
-                               else
-                                   dp[i][j]=false;
+                   else if(p.charAt(j)=='*')
+                       dp[i][j]=dp[i+1][j]|| dp[i][j+1];
+                   else
+                       dp[i][j]=false;
                }
+               
            }
        }
         return dp[0][0];
