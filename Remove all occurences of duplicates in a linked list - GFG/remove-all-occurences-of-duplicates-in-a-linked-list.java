a611edbb1feca75665db2bd1a1c085ee102b6bd5
WarisAmir <warisamir1918@yahoo.com>
2022-12-02 00:12:14 +0530

    Added solution - LeetHub

---
+++ Remove all occurences of duplicates in a linked list - GFG/remove-all-occurences-of-duplicates-in-a-linked-list.java
@@ -0,0 +1,103 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+class duplicates
+{
+    Node head;  
+    Node tail;
+	public void addToTheLast(Node node) 
+	{
+	  if (head == null) 
+	  {
+	   head = node;
+	   tail = node;
+	  } 
+	  else 
+	  {
+	   tail.next = node;
+	   tail = node;
+	  }
+	}
+      void printList(Node head)
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+           System.out.print(temp.data+" ");
+           temp = temp.next;
+        }  
+        System.out.println();
+    }
+	/* Drier program to test above functions */
+	public static void main(String args[])
+    {
+         Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();
+		 while(t>0)
+         {
+			int N = sc.nextInt();
+			duplicates llist = new duplicates(); 
+			int a1=sc.nextInt();
+			Node head= new Node(a1);
+            llist.addToTheLast(head);
+            for (int i = 1; i < N; i++) 
+			{
+				int a = sc.nextInt(); 
+				llist.addToTheLast(new Node(a));
+			}
+			
+        Solution ob = new Solution();
+		head = ob.removeAllDuplicates(llist.head);
+		llist.printList(head);
+		
+        t--;		
+        }
+    }
+    
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/* Linked List Node class
+
+class Node
+{
+    int data;
+    Node next;
+}
+    
+*/
+
+class Solution
+{
+    public static Node removeAllDuplicates(Node head)
+    {
+        //code here
+        Node temp=new Node(-1);
+       temp.next=head;
+       head=temp;
+       Node prev=temp;
+        while(prev.next!=null && prev.next.next!=null){
+           if(prev.next.data==prev.next.next.data){
+               int data=prev.next.data;
+               while(prev.next!=null && prev.next.data==data){
+                   prev.next=prev.next.next;
+               }
+           }else{
+               prev=prev.next;
+           }
+           
+       }
+       return head.next;
+          
+    }
+}
