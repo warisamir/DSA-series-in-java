abc6381afc945d13109a57386c1d0a9dfa42381a
WarisAmir <warisamir1918@yahoo.com>
2022-12-05 01:10:29 +0530

    Added solution - LeetHub

---
+++ Print Anagrams Together - GFG/print-anagrams-together.java
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main (String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	    int t=Integer.parseInt(br.readLine().trim());
+	    while(t > 0)
+	    {
+	        int n= Integer.parseInt(br.readLine().trim());
+	        String x = br.readLine().trim();
+	        String string_list[] = x.split(" ",n);
+	        
+	        Solution ob = new  Solution();
+	        
+	        List <List<String>> ans = ob.Anagrams(string_list);
+	        
+	        Collections.sort(ans, new Comparator<List<String>>(){
+            public int compare(List<String> l1, List<String> l2) {
+                    String s1 =  l1.get(0);
+                    String s2 = l2.get(0);
+                    
+                    return s1.compareTo(s2);
+                }
+            });
+	        
+	        for(int i=0;i<ans.size();i++)
+	        {
+	            for(int j=0;j<ans.get(i).size();j++)
+	            {
+	                System.out.print(ans.get(i).get(j) + " ");
+	            }
+	            System.out.println();
+	        }
+	       
+	       
+            t--;
+	    }
+	}
+    
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    public List<List<String>> Anagrams(String[] slist) {
+        // Code here
+        HashMap<String ,List<String>>hm=new HashMap<>();
+        for(String s:slist){
+            char []ch=s.toCharArray();
+            Arrays.sort(ch);
+            String st=new String(ch);
+            if(hm.containsKey(st))
+            hm.get(st).add(s);
+            else
+            {
+                hm.put(st,new ArrayList<>());
+                hm.get(st).add(s);
+            }
+        }
+        return new ArrayList<>(hm.values());
+    }
+}
