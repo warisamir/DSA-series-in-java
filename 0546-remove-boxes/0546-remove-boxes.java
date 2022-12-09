de4b4bad3ca6c554df81b3580a62d07f384ad502
WarisAmir <warisamir1918@yahoo.com>
2022-12-10 04:05:45 +0530

    Time: 36 ms (97.44%), Space: 51.3 MB (93.59%) - LeetHub

--- 0546-remove-boxes/0546-remove-boxes.java
+++ 0546-remove-boxes/0546-remove-boxes.java
@@ -5,16 +5,21 @@ class Solution {
          return helper(boxes,0,n-1,0,dp);
     }
     public int helper(int []box,int i,int j,int k,int [][][]dp){
-        if(i>j)
-            return 0;
+      if(i>j)
+          return 0;
         if(dp[i][j][k]!=0)return dp[i][j][k];
-        int res=(k+1)*(k+1)+helper(box,i+1,j,0,dp);
-        for(int m=i+1;m<=j;m++){
-             if(box[m]==box[i])
-             {
-                int fact =helper(box,i+1,m-1,0,dp)+helper(box,m,j,k+1,dp);
-                 res=Math.max(res,fact);
-             }
+        int ii=i,kk=k;
+        while(ii+1<=j && box[ii+1]==box[ii]){
+            ii++;kk++;
+        }
+        int res=(kk+1)*(kk+1)+helper(box,ii+1,j,0,dp);
+        for(int m=ii+1;m<=j;m++){
+            if(box[m]==box[ii])
+            {
+                int fac=helper(box,ii+1,m-1,0,dp)+helper(box,m,j,kk+1,dp);
+                res=Math.max(res,fac);
+                
+            }  
         }
         dp[i][j][k]=res;
         return res;
