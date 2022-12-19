089bafaed4352811261cd2786e7e5ace34dbfafd
WarisAmir <warisamir1918@yahoo.com>
2022-12-19 23:16:04 +0530

    Added solution - LeetHub

---
+++ Multiply two strings - GFG/multiply-two-strings.java
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.util.*;
+import java.math.*;
+
+class Multiply{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            String a=sc.next();
+            String b=sc.next();
+            Solution g=new Solution();
+            System.out.println(g.multiplyStrings(a,b));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public String multiplyStrings(String s1,String s2)
+    {
+        //code here.
+        BigInteger m1=new BigInteger(s1),m2=new BigInteger(s2);
+        return (m1.multiply(m2)).toString();
+    }
+}
