e28e92936607f5f0645e182b6f5cfdd2fc7e2ed7
WarisAmir <warisamir1918@yahoo.com>
2022-12-23 23:17:19 +0530

    Time: 44 ms (84.95%), Space: 47 MB (94.64%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.java
+++ 0403-frog-jump/0403-frog-jump.java
@@ -1,23 +1,23 @@
 class Solution {
     public boolean canCross(int[] arr) {
-        HashMap<Integer,HashSet<Integer>>hm=new HashMap<>();
+             HashMap<Integer,HashSet<Integer>>hm=new HashMap<>();
          for(int val:arr){
              hm.put(val,new HashSet<>());
          }
         hm.get(0).add(1);
         for(int i=0;i<arr.length;i++){
             int currst=arr[i];
-                HashSet<Integer>hs=hm.get(currst);
-            for(int jmp:hs){
+            for(int jmp:hm.get(currst)){
                 int pos=jmp+currst;
                 if(pos==arr[arr.length-1])
                     return true;
                 else if(hm.containsKey(pos)==true)
                 {
+                    HashSet<Integer>hs=hm.get(pos);
                     if(jmp-1>0)
-                        hm.get(pos).add(jmp-1);
-                    hm.get(pos).add(jmp);
-                    hm.get(pos).add(jmp+1);
+                        hs.add(jmp-1);
+                    hs.add(jmp);
+                    hs.add(jmp+1);
                 }
                 
             }
