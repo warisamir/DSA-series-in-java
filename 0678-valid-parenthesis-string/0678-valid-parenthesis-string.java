002dc1075fdb785877d9c5790fd885099e941f9e
WarisAmir <warisamir1918@yahoo.com>
2022-12-24 00:06:00 +0530

    Time: 1 ms (79.58%), Space: 40 MB (91.24%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -1,24 +1,31 @@
 class Solution {
     public boolean checkValidString(String s) {
         Stack<Integer>st=new Stack<>();
-        int ct=0;
-        int ct1=0;
-       for(int i=0;i<s.length();i++){
-           if(s.charAt(i)=='(' )
-           {ct++;
-            ct1++;
-           }
-          else if(s.charAt(i)==')')
-           { ct--;
-            ct1--;
-           }
-          else if(s.charAt(i)=='*')
-          {  ct++;ct1--;}
-           
-           if(ct<0) return false;
-           ct1=Math.max(ct1,0);
-       }
-       
-        return ct1==0;
+        Stack<Integer>st2=new Stack<>();
+        int i=0;
+        while(i<s.length()){
+            char ch=s.charAt(i);
+            if(ch=='(')
+                st.push(i);
+                else if(ch=='*')
+                    st2.push(i);
+                    else if(ch==')'){
+                      if(!st.isEmpty())
+                          st.pop();
+                        else if(!st2.isEmpty())
+                            st2.pop();
+                        else 
+                            return false;
+                    }
+            i++;
+        }
+        while(!st.isEmpty() && !st2.isEmpty())
+        {
+            if(st.peek()>st2.peek())
+                return false;
+            st.pop();
+            st2.pop();
+        }
+        return st.isEmpty();
     }
 }
