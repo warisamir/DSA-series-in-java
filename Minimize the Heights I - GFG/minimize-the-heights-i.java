57d546c1cfb54d6fcbe9f2987d2ed6eb4b85a821
WarisAmir <warisamir1918@yahoo.com>
2022-12-27 13:43:24 +0530

    Added solution - LeetHub

---
+++ Minimize the Heights I - GFG/minimize-the-heights-i.java
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int k = Integer.parseInt(inputLine[0]);
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int ans = new Solution().getMinDiff(arr, n, k);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int getMinDiff(int[] arr, int n, int k) {
+      Arrays.sort(arr);
+      int max=arr[n-1];
+      int min=arr[0];
+      int res=max-min;
+      for(int i=1;i<n;i++){
+          min=Math.min(arr[i]-k,arr[0]+k);
+          max=Math.max(arr[i-1]+k,arr[n-1]-k);
+          res=Math.min(max-min,res);
+      }
+      return res;
+    }
+}
