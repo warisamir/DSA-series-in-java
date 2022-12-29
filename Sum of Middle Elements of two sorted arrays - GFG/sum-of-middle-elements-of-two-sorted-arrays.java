692d6462e47923bb36b0293a555e38a737540430
WarisAmir <warisamir1918@yahoo.com>
2022-12-29 17:22:05 +0530

    Added solution - LeetHub

---
+++ Sum of Middle Elements of two sorted arrays - GFG/sum-of-middle-elements-of-two-sorted-arrays.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+  public class GFG {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        while (tc-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            int[] ar1 = new int[n], ar2 = new int[n];
+            String[] inputLine = br.readLine().split(" ");
+
+            for (int i = 0; i < n; i++) {
+                ar1[i] = Integer.parseInt(inputLine[i]);
+            }
+            inputLine = br.readLine().split(" ");
+            for (int i = 0; i < n; i++) {
+                ar2[i] = Integer.parseInt(inputLine[i]);
+            }
+            System.out.println(new Solution().findMidSum(ar1, ar2, n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int findMidSum(int[] ar1, int[] ar2, int n) {
+        int arr[]=new int[ar1.length+ar2.length];
+        for(int i=0;i<n;i++)
+        arr[i]=ar1[i];
+          for(int i=0;i<n;i++)
+        arr[n+i]=ar2[i];
+        Arrays.sort(arr);
+        return arr[(2*n)/2]+arr[2*(n-1)/2];
+    }
+}
