adbdb66bc21b924fb0af07dccc653e6ac0672ed7
WarisAmir <warisamir1918@yahoo.com>
2023-01-05 02:08:28 +0530

    Time: 0 ms (100.00%), Space: 43 MB (86.88%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,10 +1,14 @@
 class Solution {
     public void dfs(int[][]image,int sr,int sc,int newclr,int oldclr){
+        //boundary condition of all side of matrix 
         if(sr<0 ||sc<0 || sr>=image.length || sc>=image[0].length)
             return ;
+        //if the color is dissimilar
         if(image[sr][sc]!=oldclr)
             return;
         image[sr][sc]=newclr;
+        //moving in all direction up down right left
+        //to see the boundary
         dfs(image,sr+1,sc,newclr,oldclr);
         dfs(image,sr-1,sc,newclr,oldclr);
         dfs(image,sr,sc+1,newclr,oldclr);
