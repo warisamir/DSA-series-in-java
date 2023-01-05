1184a59d02bd2ae281b879f548c440c9c51a76ac
WarisAmir <warisamir1918@yahoo.com>
2023-01-05 17:33:38 +0530

    Time: 3 ms (93.56%), Space: 43.3 MB (48.54%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,19 +1,38 @@
+class Trie{
+    Trie child[]=new Trie[26];
+    boolean eow=false;
+}
 class Solution {
+    Trie root=new Trie();;
     public boolean wordBreak(String s, List<String> b) {
-        HashSet<String>hs=new HashSet<>(b);
-        
+      for(String s1:b){
+          add(s1);
+      } 
         boolean dp[]=new boolean[s.length()+1];
         dp[0]=true;
-          
-        for(int i =1;i<=s.length();i++){//~
-            for(int j=0;j<i;j++){
-                 // System.out.println(s.substring(j,i));
-                if(dp[j] && hs.contains(s.substring(j,i))){
-                    dp[i] = true;
-                    break;
-                }  
+        
+        for(int i=0;i<s.length();i++){
+            if(!dp[i])
+                continue;
+            int j=i;
+            Trie curr=root;
+            while(j<s.length()&& curr.child[s.charAt(j)-'a']!=null)
+            {
+                curr=curr.child[s.charAt(j)-'a'];
+                j+=1;
+                if(curr.eow==true)
+                    dp[j]=true;
             }
         }
         return dp[s.length()];
        }
+public void add(String s){
+    Trie node=root;
+    for(char ch:s.toCharArray()){
+        if(node.child[ch-'a']==null)
+            node.child[ch-'a']=new Trie();
+        node=node.child[ch-'a'];
+    }
+    node.eow=true;
+}
 }
