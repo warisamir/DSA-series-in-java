7b8660dc12cefafcd70927e0b196df59dde76643
WarisAmir <warisamir1918@yahoo.com>
2023-01-05 16:39:44 +0530

    Time: 19 ms (26.24%), Space: 46.9 MB (34.47%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,21 +1,19 @@
 class Solution {
     public boolean wordBreak(String s, List<String> b) {
-        HashSet<String>hs=new HashSet<>();
-        for(String s1:b){
-            hs.add(s1);
-        }
-        boolean dp[]=new boolean[s.length()];
-        for(int i=0;i<dp.length;i++){
-            if(hs.contains(s.substring(0,i+1)))
-                dp[i]=true;
-            else{
-                for(int j=0;j<i;j++)
-                {
-                    if(dp[j]==true && hs.contains(s.substring(j+1,i+1)))
-                        dp[i]=true;
-                }
+        HashSet<String>hs=new HashSet<>(b);
+        
+        boolean dp[]=new boolean[s.length()+1];
+        dp[0]=true;
+          
+        for(int i =1;i<=s.length();i++){//~
+            for(int j=0;j<i;j++){
+                 // System.out.println(s.substring(j,i));
+                if(dp[j] && hs.contains(s.substring(j,i))){
+                    dp[i] = true;
+                    break;
+                }  
             }
         }
-        return dp[dp.length-1];
-    }
+        return dp[s.length()];
+       }
 }
