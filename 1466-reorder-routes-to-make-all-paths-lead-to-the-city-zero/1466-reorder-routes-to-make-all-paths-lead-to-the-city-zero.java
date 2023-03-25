da62258eed89cd80ecee9cf8e5b0305c87d98693
WarisAmir <warisamir1918@gmail.com>
2023-03-25 14:29:50 +0530

    Time: 46 ms (80.44%), Space: 69.1 MB (66.82%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -2,11 +2,11 @@ class Solution {
     class Edge {
     int src;
     int nbr;
-    boolean check;
-    Edge(int src, int nbr, boolean check) {
+    int a;
+    Edge(int src, int nbr,int a) {
         this.src = src;
         this.nbr = nbr;
-        this.check = check;
+        this.a = a;
     }
 }
     public int minReorder(int n, int[][] connections) {
@@ -16,8 +16,8 @@ class Solution {
             arr[i]=new ArrayList<Edge>();
         }
         for(int con[]:connections){
-           arr[con[0]].add(new Edge(con[0],con[1],true));
-            arr[con[1]].add(new Edge(con[1],con[0],false));
+           arr[con[0]].add(new Edge(con[0],con[1],1));
+            arr[con[1]].add(new Edge(con[1],con[0],-1));
         }
        return  dfs(arr,0,vis);
     }
@@ -26,13 +26,13 @@ class Solution {
     vis[src] = true;
     for(Edge e: adj[src]){
         if(!vis[e.nbr]){
-            if(e.check){
+            if(e.a==1){
                 ct++;
             }
             ct += dfs(adj, e.nbr, vis);
         }
-    }
-    
-    return ct; 
+    }return ct;
+        
     }
 }
+    
