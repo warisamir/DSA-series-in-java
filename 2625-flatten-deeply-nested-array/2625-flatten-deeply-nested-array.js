de9c4c0470babac5ca56bc04cffbc15d80bab244
WarisAmir <warisamir1918@gmail.com>
2023-04-26 05:15:32 +0530

    Time: 166 ms (69.69%), Space: 99.2 MB (5.38%) - LeetHub

--- 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
+++ 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
@@ -6,11 +6,19 @@
 var flat = function (arr, n) {
     if(n==0) return arr;
     let res=[];
-    for(let k of arr){
-        if(Array.isArray(k))
-            res.push(...flat(k,n-1))
-        else
-            res.push(k);
+     for(let i=0; i<arr.length; i++){
+        
+        // check if element is instance of array and depth is not equal to 0
+        if(n>0 && Array.isArray(arr[i])){
+            
+            // recursively call the function for this array and push the flattened array to the answer array
+            res.push(...flat(arr[i], n-1));
+        }
+        // else directy push the current array
+        else{
+            res.push(arr[i]);
+        }
     }
+    
     return res;
 }
