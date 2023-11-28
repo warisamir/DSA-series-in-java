73602711db493665d95a934eb7ced5ac4fea7840
WarisAmir <warisamir1918@gmail.com>
2023-11-28 12:00:09 +0530

    Time: 3 ms (95.08%), Space: 44.3 MB (39.40%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,34 +1,28 @@
 class Solution {
-    public boolean canFinish(int n, int[][] adj) {
-        
-        //it cycle is detected then schedule is not possible
-        //if acyclic -> true;
-        //wherer incoming is not zero for a graph 
-        
-        //if topological sort is not possible if cyclic 
-        // boolean vis[]=new boolean[n];
-        ArrayList<Integer>[]ad=new ArrayList[n];
+    public boolean canFinish(int n, int[][] edge) {
+        ArrayList<Integer>[]adj=new ArrayList[n];
         for(int i=0;i<n;i++){
-            ad[i]=new ArrayList<>();
+            adj[i]=new ArrayList<>();
         }
-        for(int edge[]:adj){
-           ad[edge[0]].add(edge[1]);
-            // ad[edge[1]].add(edge[0]);
+        for(int e[]:edge){
+            adj[e[0]].add(e[1]);
         }
         int vis[]=new int[n];
         Arrays.fill(vis,-1);
         for(int i=0;i<n;i++){
-            if(vis[i]==-1 && dfs(i,ad,vis)==true)return false;
+            if(vis[i]==-1 && dfs(i,vis,adj)==true)
+                return false;
         }
         return true;
     }
-    
-    public boolean dfs(int src,ArrayList<Integer>[]adj,int vis[]){
-        if(vis[src]==0)return true;
+    private boolean dfs(int src,int []vis,ArrayList<Integer>[]adj){
+        if(vis[src]==0)
+            return true;
         if(vis[src]==1) return false;
         vis[src]=0;
-        for(Integer nbr:adj[src]){
-            if(dfs(nbr,adj,vis)==true)
+        for(int nbr:adj[src])
+        {
+            if(dfs(nbr,vis,adj)==true)
                 return true;
         }
         vis[src]=1;
