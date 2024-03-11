1434e2b9911dd4f9f1d9fb4dcfc171611ea06bf8
WarisAmir <warisamir1918@gmail.com>
2024-03-12 05:08:17 +0530

    Time: 2 ms (62.85%), Space: 41.4 MB (75.96%) - LeetHub

---
+++ 0791-custom-sort-string/0791-custom-sort-string.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String customSortString(String order, String s) {
+         Map<Character, Integer> sCharFreqMp = new HashMap<>();
+        for (char currChr : s.toCharArray()) {
+            sCharFreqMp.put(currChr, sCharFreqMp.getOrDefault(currChr, 0) + 1);
+        }
+        
+        StringBuilder res = new StringBuilder();
+        
+        for (char currChr : order.toCharArray()) {
+            if (sCharFreqMp.containsKey(currChr)) {
+                int freq = sCharFreqMp.get(currChr);
+                while (freq-- > 0) {
+                    res.append(currChr);
+                }
+                sCharFreqMp.put(currChr, 0);
+            }
+        }
+        
+        for (char currChr : s.toCharArray()) {
+            if (sCharFreqMp.get(currChr) != 0) {
+                res.append(currChr);
+                sCharFreqMp.put(currChr, sCharFreqMp.get(currChr) - 1);
+            }
+        }
+        
+        return res.toString();
+    
+    }
+}
